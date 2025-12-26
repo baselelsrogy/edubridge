20be4585a06642ef56089eecfa5e14313e31096b
baselelsrogy <baselhany88@gmail.com>
2025-12-27 00:03:42 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,38 @@
+import { FaArrowRight } from 'react-icons/fa';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header id="header">
+      <nav className="navbar">
+        <a href="#" className="logo">
+          EduBridge
+        </a>
+
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Product</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+
+        <div className="account">
+          <a href="#" className="login">
+            Login
+          </a>
+          <a href="#" className="btn-join">
+            Join Us
+            <FaArrowRight />
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
